43e83896813a4be5d1252351941d8e98488dcf34
Jurie Horneman <jhorneman@pobox.com>
2012-12-05 17:58:14 +0100

    Boostrap-editable experiment.

--- gameconfs/static/js/gameconfs.js
+++ gameconfs/static/js/gameconfs.js
@@ -1,3 +1,31 @@
  $(document).ready(function() {
-   $('.datepicker').datepicker({'format':'dd/mm/yyyy', 'weekStart':1})
+     $('#event_name').editable({
+         type: 'text',
+         pk: 1,
+         url: 'post.php',
+         title: 'Enter username'
+     });
+
+     $('#event_url').editable({
+         type: 'text',
+         pk: 1,
+         url: 'post.php',
+         title: 'Enter username'
+     });
+
+     $('#event_hashtags').editable({
+         type: 'text',
+         pk: 1,
+         url: 'post.php',
+         title: 'Enter username'
+     });
+
+     $('#event_twitter').editable({
+         type: 'text',
+         pk: 1,
+         url: 'post.php',
+         title: 'Enter username'
+     });
+
+//     event_date
  });
