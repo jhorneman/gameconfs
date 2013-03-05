32989127672d91b3dfbfeb15989cee92fc21c38c
Jurie Horneman <jhorneman@pobox.com>
2013-03-05 01:53:06 +0100

    Updated Bootstrap date picker, made it work properly in event editing form.

--- gameconfs/static/js/event-editing.js
+++ gameconfs/static/js/event-editing.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
-    $('.datepicker').datepicker({'format':'dd/mm/yyyy', 'weekStart':1})
+    var startDatePickerData,
+        endDatePickerData;
+
+    startDatePickerData = $('#start_date')
+        .datepicker({
+            'format': 'dd/mm/yyyy',
+            'weekStart': 1
+        })
+        .on('changeDate', function(evt) {
+            if (evt.date.valueOf() > endDatePickerData.date.valueOf()) {
+                endDatePickerData.setValue(new Date(evt.date));
+            }
+            endDatePickerData.fill();       // To re-evaluate disabled state of days
+            startDatePickerData.hide();
+            $('#end_date')[0].focus();
+        })
+        .data("datepicker");
+
+    endDatePickerData = $('#end_date')
+        .datepicker({
+            'format': 'dd/mm/yyyy',
+            'weekStart': 1,
+            'onRender': function(date) {
+                return date.valueOf() < startDatePickerData.date.valueOf() ? 'disabled' : '';
+            }
+        })
+        .on('changeDate', function(evt) {
+            if (evt.date.valueOf() < startDatePickerData.date.valueOf()) {
+                endDatePickerData.setValue(new Date(startDatePickerData.date));
+            }
+            endDatePickerData.hide();
+        })
+        .data("datepicker");
 });
