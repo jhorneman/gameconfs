7f6d0a9d9585685898fa8a9a77d59dafb3961c17
Jurie Horneman <jhorneman@pobox.com>
2013-11-24 13:43:57 +0100

    Autocomplete experiment.

---
+++ gameconfs/static/js/gameconfs.js
@@ -0,0 +1,29 @@
+ $.widget( "custom.catcomplete", $.ui.autocomplete, {
+    _renderMenu: function( ul, items ) {
+      var that = this,
+        currentCategory = "";
+      $.each( items, function( index, item ) {
+        if ( item.category != currentCategory ) {
+          ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+          currentCategory = item.category;
+        }
+        that._renderItemData( ul, item );
+      });
+    }
+  });
+
+$(function() {
+var data = [
+  { label: "London", category: "Cities" },
+  { label: "San Francisco", category: "Cities" },
+  { label: "Amsterdam", category: "Cities" },
+  { label: "France", category: "Countries" },
+  { label: "California", category: "States" },
+  { label: "Europe", category: "Continents" }
+];
+
+$( "#search" ).catcomplete({
+  delay: 0,
+  source: data
+});
+});
