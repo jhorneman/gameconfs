abdd84c00a213eb3b0ccb68ec99e6d3ced4e1878
Jurie Horneman <jhorneman@pobox.com>
2014-01-04 00:33:27 +0100

    Fixed uWSGI start script, using new run mode for vagrant testing.

--- gameconfs/uwsgi.py
+++ gameconfs/uwsgi.py
@@ -1,6 +1,6 @@
-from * import create_app
+from gameconfs import create_app
 from gzip_middleware import GzipMiddleware
 
-flask_app, db = create_app("production")
+flask_app, db = create_app("vagrant-test")
 
 app = GzipMiddleware(flask_app, compresslevel=5)
