8ad86a7e4bced86da5b36790aa7ec31cbe5afafc
Jurie Horneman <jhorneman@pobox.com>
2014-01-14 21:38:31 +0100

    Switched query order in filter_by_place_name() so most common query (country) comes first.

--- gameconfs/helpers.py
+++ gameconfs/helpers.py
@@ -73,17 +73,17 @@ def get_year_range():
 
 
 def filter_by_place_name(_query, _place_name):
-    continent = Continent.query. \
-        filter(Continent.name.ilike(_place_name)). \
-        first()
-    if continent:
-        return _query.filter(Continent.id == continent.id), continent.name
-
     country = Country.query. \
         filter(Country.name.ilike(_place_name)). \
         first()
     if country:
         return _query.filter(Country.id == country.id), country.name
+
+    continent = Continent.query. \
+        filter(Continent.name.ilike(_place_name)). \
+        first()
+    if continent:
+        return _query.filter(Continent.id == continent.id), continent.name
 
     state = State.query. \
         filter(State.name.ilike(_place_name)). \
