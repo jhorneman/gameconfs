c28720f696d1b1fd6ef922f1c071f242a68bedd8
Jurie Horneman <jhorneman@pobox.com>
2014-01-16 16:48:55 +0100

    Upgraded Flask and extensions, fixed Flask-Wtf problem due to upgrade.

--- gameconfs/forms.py
+++ gameconfs/forms.py
@@ -1,13 +1,15 @@
-from flask.ext.wtf import Form, TextField, TextAreaField, Required, ValidationError
+from flask.ext.wtf import Form
+from wtforms import TextField
+from wtforms.validators import InputRequired, ValidationError
 from flask.ext.wtf.html5 import URLField, DateField
 from datetime import date
 
 
 class EventForm(Form):
-    name = TextField('Name', validators=[Required()])
-    start_date = DateField('Start date', validators=[Required()], default=date.today())
-    end_date = DateField('End date', validators=[Required()], default=date.today())
-    event_url = URLField('URL', validators=[Required()])
+    name = TextField('Name', validators=[InputRequired()])
+    start_date = DateField('Start date', validators=[InputRequired()], default=date.today())
+    end_date = DateField('End date', validators=[InputRequired()], default=date.today())
+    event_url = URLField('URL', validators=[InputRequired()])
     twitter_hashtags = TextField('Hashtags')
     twitter_account = TextField('Twitter account')
     venue = TextField('Venue')
