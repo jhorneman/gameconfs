3626c59dceeb9f0c94a163405f89c26e47ccdf12
Jurie Horneman <jhorneman@pobox.com>
2014-07-30 22:59:23 +0200

    Added tracking of outbound and sponsoring links.

--- gameconfs/static/js/main.js
+++ gameconfs/static/js/main.js
@@ -15,3 +15,24 @@
         });
     };
 }(jQuery, window));
+
+/**
+* Function that tracks a click on an outbound link in Google Analytics.
+* This function takes a valid URL string as an argument, and uses that URL string
+* as the event label.
+*/
+var trackOutboundLink = function(url) {
+   ga('send', 'event', 'outbound', 'click', url, {'hitCallback':
+     function () {
+     document.location = url;
+     }
+   });
+}
+
+var trackSponsorLink = function(url) {
+   ga('send', 'event', 'sponsor', 'click', url, {'hitCallback':
+     function () {
+     document.location = url;
+     }
+   });
+}
