f6283e88bf33ef2c7f388dce1a85d571dc36b199
Jurie Horneman <jurie@jurie.org>
2015-12-05 12:49:44 +0100

    Upgraded some more packages.

--- gameconfs/default_config.py
+++ gameconfs/default_config.py
@@ -2,9 +2,12 @@
 SECRET_KEY = '\xa7Uy\tPMY\x99\xc4\xa1\x8bB\x9a\x8d\xc9T\xd3\xe3\xa9\xd7b\xbf\x0e\x81'
 DEBUG = False
 
-# Flask Mail
+# Flask-Mail
 MAIL_DEFAULT_SENDER = 'admin@gameconfs.com'
 
-# Flask Cache
+# Flask-Cache
 CACHE_KEY_PREFIX = 'GC:'
 CACHE_NO_NULL_WARNING = True
+
+# Flask-SQLAlchemy
+SQLALCHEMY_TRACK_MODIFICATIONS = False
