312868f838a371319642e7c843cb67a782d85bac
Jurie Horneman <jurie@jurie.org>
2015-12-05 17:49:06 +0100

    Changed TextField to StringField, added boolean for published status.

--- gameconfs/forms.py
+++ gameconfs/forms.py
@@ -1,21 +1,22 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, HiddenField
+from wtforms import StringField, HiddenField, BooleanField
 from wtforms.validators import InputRequired, ValidationError
 from flask.ext.wtf.html5 import URLField, DateField
 from datetime import date
 
 
 class EventForm(Form):
-    name = TextField('Name', validators=[InputRequired()])
+    name = StringField('Name', validators=[InputRequired()])
     start_date = DateField('Start date', validators=[InputRequired()], default=date.today())
     end_date = DateField('End date', validators=[InputRequired()], default=date.today())
     event_url = URLField('URL', validators=[InputRequired()])
-    twitter_hashtags = TextField('Hashtags')
-    twitter_account = TextField('Twitter account')
-    venue = TextField('Venue')
-    address = TextField('City')
-    series = TextField('Series')
+    twitter_hashtags = StringField('Hashtags')
+    twitter_account = StringField('Twitter account')
+    venue = StringField('Venue')
+    address = StringField('City')
+    series = StringField('Series')
     city_id = HiddenField()
+    is_published = BooleanField('Is published')
 
     def validate_end_date(form, field):
         if field.data < form.start_date.data:
@@ -23,4 +24,4 @@ class EventForm(Form):
 
 
 class SearchForm(Form):
-    search_string = TextField('Query')
+    search_string = StringField('Query')
