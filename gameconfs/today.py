828477b8dd9417b880e96fc667e0d36005d7cc01
Jurie Horneman <jurie@jurie.org>
2015-12-06 18:10:43 +0100

    Make sure we can take datetimes and return the right type.

--- gameconfs/today.py
+++ gameconfs/today.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from datetime import date, datetime
+import datetime
 
 
 __all__ = ["get_today", "get_now", "override_today"]
@@ -11,14 +11,17 @@ today_override = None
 
 def get_today():
     global today_override
-    return today_override if today_override else date.today()
+    return today_override if today_override else datetime.date.today()
 
 
 def get_now():
     global today_override
-    return datetime(today_override.year, today_override.month, today_override.day) if today_override else datetime.now()
+    return datetime.datetime(today_override.year, today_override.month, today_override.day)\
+        if today_override else datetime.datetime.now()
 
 
 def override_today(_new_today):
     global today_override
+    if isinstance(_new_today, datetime.datetime):
+        _date = datetime.date(_new_today.year, _new_today.month, _new_today.day)
     today_override = _new_today
