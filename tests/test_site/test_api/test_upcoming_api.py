9a53e9af32e49d954d5afd4d02e99af5b205db70
Jurie Horneman <jurie@jurie.org>
2015-12-06 17:38:59 +0100

    Refactored, wrote note notes for more tests.

--- tests/test_site/test_api/test_upcoming_api.py
+++ tests/test_site/test_api/test_upcoming_api.py
@@ -7,72 +7,86 @@ from . import base_url, test_event
 
 
 class UpcomingEventsAPITestCase(SiteTestCase):
-    def call_api(self, _params):
-        return self.c.get(base_url + "v1/upcoming", query_string=_params)
-    
+    def call_api(self, _params, _expected_status):
+        r = self.c.get(base_url + "v1/upcoming", query_string=_params)
+        assert r.status_code == _expected_status, "Expected status code to be {0}, got {1}.".format(_expected_status, r.status_code)
+        if _expected_status == 405:
+            return None
+        data = json.loads(r.data)
+        if _expected_status == 200:
+            assert "message" not in data
+            assert len(data["results"]) == data["nrFoundEvents"]
+        return data
+
     def test_post_returns_405(self):
         r = self.c.post(base_url + "v1/upcoming", query_string={})
         assert r.status_code == 405
 
-    def test_no_data_fails(self):
-        r = self.call_api({})
-        assert r.status_code == 200
-        data = json.loads(r.data)
-        assert "message" not in data
-        eq_(data["foundLocationName"], None)
-        assert data["nrFoundEvents"] > 0
-        assert len(data["results"]) == data["nrFoundEvents"]
-
     def test_wrong_data_fails(self):
-        r = self.call_api({"blah": 0})
-        assert r.status_code == 200
-        data = json.loads(r.data)
-        assert "message" not in data
-        eq_(data["foundLocationName"], None)
-        assert data["nrFoundEvents"] > 0
-        assert len(data["results"]) == data["nrFoundEvents"]
+        data = self.call_api({"blah": 0}, 400)
+        assert data["message"].startswith("Did not recognize parameter")
 
-    def test_nr_months_wrong_format(self):
-        r = self.call_api({"nrMonths": "FIVE"})
-        assert r.status_code == 400
-        data = json.loads(r.data)
+    def test_nr_months_wrong_format_fails(self):
+        data = self.call_api({"nrMonths": "FIVE"}, 400)
         assert data["message"].startswith("Could not parse nrMonths value")
 
-    def test_nr_months_illegal_values(self):
-        r = self.call_api({"nrMonths": -1})
-        assert r.status_code == 400
-        data = json.loads(r.data)
+    def test_nr_months_illegal_values_fails(self):
+        data = self.call_api({"nrMonths": -1}, 400)
         eq_(data["message"], "nrMonths must be at least 1.")
-    
-        r = self.call_api({"nrMonths": 0})
-        assert r.status_code == 400
-        data = json.loads(r.data)
+
+        data = self.call_api({"nrMonths": 0}, 400)
         eq_(data["message"], "nrMonths must be at least 1.")
-    
-        r = self.call_api({"nrMonths": 13})
-        assert r.status_code == 400
-        data = json.loads(r.data)
+
+        data = self.call_api({"nrMonths": 13}, 400)
         eq_(data["message"], "nrMonths may not be higher than 12.")
 
-    def test_nr_months_legal_values(self):
-        r = self.call_api({"nrMonths": 1})
-        assert r.status_code == 200
-        data = json.loads(r.data)
-        assert "message" not in data
-        eq_(data["foundLocationName"], None)
-        assert data["nrFoundEvents"] > 0
-        assert len(data["results"]) == data["nrFoundEvents"]
-    
-        r = self.call_api({"nrMonths": 12})
-        assert r.status_code == 200
-        data = json.loads(r.data)
-        assert "message" not in data
-        eq_(data["foundLocationName"], None)
-        assert data["nrFoundEvents"] > 0
-        assert len(data["results"]) == data["nrFoundEvents"]
-
-    def test_place_empty(self):
-        r = self.call_api({"place": " "})
-        assert r.status_code == 400
-        data = json.loads(r.data)
+    def test_place_empty_fails(self):
+        data = self.call_api({"place": " "}, 400)
         eq_(data["message"], "Place argument was empty.")
+
+    # TO TEST
+
+    # No parameters (3 months + no place) returns the right event(s)
+
+    # 1 month + no place returns the right event(s)
+    # 12 months + no place returns the right event(s)
+
+    # No month + place returns the right event(s)
+
+    # A month + place returns the right event(s)
+
+    # A month + "other" returns the right event(s)
+
+    # NEVER an unpublished event
+
+    # A unpublished event in Europe at 1 month out
+    # B unpublished event in Asia at 1 month out
+    # C unpublished event in Other at 1 month out
+
+    # D published event in Europe at 1 month out
+    # E published event in Asia at 1 month out
+    # F published event in Other at 1 month out
+
+    # G published event in Europe at 3 months out
+    # H published event in Asia at 3 months out
+
+    # I published event in Europe at 12 months out
+    # J published event in Asia at 12 months out
+
+    # K published event in Europe at 13 months out
+    # L published event in Asia at 13 months out
+
+    def test_nr_months_legal_values_succeeds(self):
+        data = self.call_api({"nrMonths": 1}, 200)
+        eq_(data["foundLocationName"], None)
+        assert data["nrFoundEvents"] > 0
+
+        data = self.call_api({"nrMonths": 12}, 200)
+        eq_(data["foundLocationName"], None)
+        assert data["nrFoundEvents"] > 0
+
+    # def test_wrong_data_fails(self):
+    #     data = self.call_api({"blah": 0}, 200)
+    #     eq_(data["foundLocationName"], None)
+    #     assert data["nrFoundEvents"] > 0
+    #     assert len(data["results"]) == data["nrFoundEvents"]
