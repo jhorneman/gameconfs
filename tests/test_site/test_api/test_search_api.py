1967b003f55b40e1e984263ffa0753acf067bc9b
Jurie Horneman <jurie@jurie.org>
2015-12-08 13:58:03 +0100

    Adapted tests to new mock event data.

--- tests/test_site/test_api/test_search_api.py
+++ tests/test_site/test_api/test_search_api.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from nose.tools import *
-from . import APITestCase, test_event
+from . import APITestCase
 
 
 class SearchAPITestCase(APITestCase):
@@ -21,13 +21,14 @@ class SearchAPITestCase(APITestCase):
         assert data["message"].startswith("Did not recognize parameter")
 
     def test_right_date(self):
-        data = self.call_api({"date": test_event["startDate"]}, 200)
+        data = self.call_api({"date": "2014-06-01"}, 200)
         eq_(data["foundLocationName"], None)
         assert data["nrFoundEvents"] > 0
         result = data["results"][0]
-        eq_(result["name"], test_event["name"])
-        eq_(result["city"], test_event["city"])
-        eq_(result["country"], test_event["country"])
+        eq_(result["name"], "Europe 1 month out")
+        eq_(result["city"], "Paris")
+        eq_(result["country"], "France")
+        eq_(result["continent"], "Europe")
 
     def test_date_wrong_format(self):
         data = self.call_api({"date": "May 5th 2012"}, 400)
@@ -38,27 +39,27 @@ class SearchAPITestCase(APITestCase):
         eq_(data["message"], "Can't search in the past.")
 
     def test_start_date_but_no_end_date(self):
-        data = self.call_api({"startDate": test_event["startDate"]}, 400)
+        data = self.call_api({"startDate": "2014-06-01"}, 400)
         eq_(data["message"], "Found start date argument but no end date.")
 
     def test_end_date_but_no_start_date(self):
-        data = self.call_api({"endDate": test_event["startDate"]}, 400)
+        data = self.call_api({"endDate": "2014-06-01"}, 400)
         eq_(data["message"], "Found end date argument but no start date.")
 
     def test_start_date_wrong_format(self):
-        data = self.call_api({"startDate": "May 5th 2012", "endDate": test_event["endDate"]}, 400)
+        data = self.call_api({"startDate": "May 5th 2012", "endDate": "2014-06-01"}, 400)
         assert data["message"].startswith("Couldn't parse date")
 
     def test_start_date_in_past(self):
-        data = self.call_api({"startDate": "2012-01-01", "endDate": test_event["endDate"]}, 400)
+        data = self.call_api({"startDate": "2012-01-01", "endDate": "2014-06-01"}, 400)
         eq_(data["message"], "Can't search in the past.")
 
     def test_end_date_wrong_format(self):
-        data = self.call_api({"startDate": test_event["startDate"], "endDate": "May 5th 2012"}, 400)
+        data = self.call_api({"startDate": "2014-06-01", "endDate": "May 5th 2012"}, 400)
         assert data["message"].startswith("Couldn't parse date")
 
     def test_end_date_before_start_date(self):
-        data = self.call_api({"startDate": test_event["endDate"], "endDate": test_event["startDate"]}, 400)
+        data = self.call_api({"startDate": "2014-06-01", "endDate": "2014-05-23"}, 400)
         eq_(data["message"], "End date may not be before start date.")
 
     def test_event_name_empty(self):
@@ -70,12 +71,12 @@ class SearchAPITestCase(APITestCase):
         eq_(data["message"], "Place argument was empty.")
 
     def test_place_with_results(self):
-        data = self.call_api({"place": "Vienna"}, 200)
-        eq_(data["foundLocationName"], "Vienna")
-        assert data["nrFoundEvents"] > 0
+        data = self.call_api({"place": "Paris"}, 200)
+        eq_(data["foundLocationName"], "Paris")
+        eq_(data["nrFoundEvents"], 4)
         for result in data["results"]:
-            eq_(result["city"], "Vienna")
-            eq_(result["country"], "Austria")
+            eq_(result["city"], "Paris")
+            eq_(result["country"], "France")
             eq_(result["continent"], "Europe")
 
     def test_place_with_no_results(self):
