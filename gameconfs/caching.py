c7a3bc8865ebc34dda5fcdbcbb82e5a7e5cc44f7
Jurie Horneman <jurie@jurie.org>
2015-12-14 19:25:41 +0100

    More logging to track down memcached error.

--- gameconfs/caching.py
+++ gameconfs/caching.py
@@ -40,6 +40,7 @@ class InterfaceFixCacheClient(bmemcached.Client):
                         raise e
                     else:
                         logger.error("memcached error in method {0}: {1}.".format(name, str(e)))
+                        logger.error("First argument: {0}({1}).".format(args[0], type(args[0])))
                         result = None
 
                 return result
