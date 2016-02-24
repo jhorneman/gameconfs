0f1817ec45187815042c2a521f1c7a75e50f2cef
Jurie Horneman <jurie@jurie.org>
2016-02-24 15:53:15 +0100

    Added tag line, meta description.
    Passing tag line, meta description, and admin email through to templates.

--- gameconfs/project.py
+++ gameconfs/project.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
 PROJECT_NAME = "Gameconfs"
+
+PROJECT_TAG_LINE = "a directory of game industry events"
+
+PROJECT_META_DESCRIPTION = "The world's most comprehensive directory of conferences, trade shows and events related to electronic games."
+
 PROJECT_PREFIX = "GC"
+
 ADMIN_EMAIL = "admin@gameconfs.com"
--- gameconfs/views.py
+++ gameconfs/views.py
@@ -18,7 +18,7 @@ from gameconfs.query_helpers import *
 from gameconfs.security import editing_kill_check, user_can_edit
 from today import get_today, get_now
 from kill_switches import is_feature_on
-from project import PROJECT_NAME
+from project import PROJECT_NAME, PROJECT_TAG_LINE, PROJECT_META_DESCRIPTION, ADMIN_EMAIL
 
 
 def get_request_parameters():
@@ -65,6 +65,9 @@ def mailto(_address, _subject=None, _body=None):
 def inject_common_values():
     common_values = {
         "project_name": PROJECT_NAME,
+        "tag_line": PROJECT_TAG_LINE,
+        "meta_description": PROJECT_META_DESCRIPTION,
+        "admin_email": ADMIN_EMAIL,
         "logged_in": user_can_edit(),
         "sponsor": None,
         "ce_retarget": is_feature_on(app, "CE_RETARGET"),
