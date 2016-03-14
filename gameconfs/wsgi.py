f5c04d0b2f6de85428cfe67bf3e6104c1c47b2f0
Jurie Horneman <jurie@jurie.org>
2016-03-14 20:03:59 +0100

    Calling create_app with name of production config file.

--- gameconfs/wsgi.py
+++ gameconfs/wsgi.py
@@ -1,6 +1,6 @@
 from . import create_app
 from gzip_middleware import GzipMiddleware
 
-flask_app, db = create_app("production")
+flask_app, db = create_app("production", "gameconfs-heroku.py")
 
 app = GzipMiddleware(flask_app)
